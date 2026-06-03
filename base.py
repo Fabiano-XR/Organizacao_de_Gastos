a207a16048141926758d9443ff94198b7d3b9f94
Fabiano XR <fabiano.xr12970@gmail.com>
2026-06-02 21:14:44 -0300

    função excluir gasto

--- base.py
+++ base.py
@@ -1,25 +1,39 @@
-salario = total = 0
+salario = total = 0.0
 gastos = []
 exe = 1
-i = 1
 def alt_salario():
-    cache = float(input("Digite o salario (ou 0 para voltar): "))
+    while True:
+        try:
+            cache = float(input("Digite o salario (ou 0 para voltar): "))
+            break
+        except:
+            print("Entrada invalida")
     if cache != 0:
         salario = cache
         return salario
-
 def add_gasto():
     cache = []
     while True:
-        temp = float(input("Digite o novo gasto (ou 0 para encerrar): "))
+        while True:
+            try:
+                temp = float(input("Digite o novo gasto (ou 0 para encerrar): "))
+                break
+            except:
+                print("Entrada invalida")
+
         if temp == 0:
             break
         cache.append(temp)
     return cache
-
+def list_gasto():
+    i = 1
+    for gasto in gastos:
+        print(f"{i} - {gasto}")
+        i+=1
+    print(f"Gasto Total = R${total}")
 
 while True:
-    exe = int(input("\n 0 - Encerrar. \n 1 - Alterar o salario. \n 2 - Adicionar gasto. \n 3 - Mostrar atual salario. \n 4 - Mostrar atuais gastos. \n Digite oque deseja fazer? "))
+    exe = int(input("\n 0 - Encerrar. \n 1 - Alterar o salario. \n 2 - Adicionar gasto. \n 3 - Mostrar atual salario. \n 4 - Mostrar atuais gastos. \n 5 - Excluir um gasto. \n Digite oque deseja fazer? "))
     print("\n")
 
     if (exe == 0):
@@ -34,10 +48,12 @@ while True:
         print(f"O atual salario é {salario:.2f}")
     elif (exe == 4):
         print("Os atuais gastos são:")
-        for gasto in gastos:
-            print(f"{i} - {gasto}")
-            i+=1
-        print(f"Gasto Total = R${total}")
-        i = 1
+        list_gasto()
+    elif (exe == 5):
+        list_gasto()
+        delete = int(input("\n Qual gasto gostaria de exluir? "))
+        total -= gastos.pop((delete-1))
+        print("Gastos atuais:")
+        list_gasto()
     else:
         print("Opção Inexistente.")
