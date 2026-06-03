65471b983f468f9cb7a042acbff36ac54f0056d2
Fabiano XR <fabiano.xr12970@gmail.com>
2026-06-02 21:26:00 -0300

    add anti-erros

--- base.py
+++ base.py
@@ -33,27 +33,32 @@ def list_gasto():
     print(f"Gasto Total = R${total}")
 
 while True:
-    exe = int(input("\n 0 - Encerrar. \n 1 - Alterar o salario. \n 2 - Adicionar gasto. \n 3 - Mostrar atual salario. \n 4 - Mostrar atuais gastos. \n 5 - Excluir um gasto. \n Digite oque deseja fazer? "))
-    print("\n")
+    while True:
+        try:
+            exe = int(input("\n 0 - Encerrar. \n 1 - Alterar o salario. \n 2 - Adicionar gasto. \n 3 - Mostrar atual salario. \n 4 - Mostrar atuais gastos. \n 5 - Excluir um gasto. \n Digite oque deseja fazer? "))
+            break
+        except:
+            print("Entrada invalida. \n")
+        print("\n")
 
-    if (exe == 0):
+    if (exe == 0): #encerra programa
         break
-    elif (exe == 1):
+    elif (exe == 1): #altera o salario
         salario = alt_salario()
-    elif (exe == 2):
+    elif (exe == 2): #adiciona um gasto
         gastos.extend(add_gasto())
         for gasto in gastos:
             total += gasto
-    elif (exe == 3):
+    elif (exe == 3): #mostra o salario
         print(f"O atual salario é {salario:.2f}")
-    elif (exe == 4):
+    elif (exe == 4): #mostra os gastos
         print("Os atuais gastos são:")
         list_gasto()
-    elif (exe == 5):
+    elif (exe == 5): #exclui um gasto
         list_gasto()
         delete = int(input("\n Qual gasto gostaria de exluir? "))
         total -= gastos.pop((delete-1))
         print("Gastos atuais:")
         list_gasto()
-    else:
+    else: #se digitar um numero errado
         print("Opção Inexistente.")
